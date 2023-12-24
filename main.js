434b1c03159bcc54871315ba5c120483fcae8ed8
Gabriel <gabriel@mazure.fr>
2023-12-24 10:19:12 +0100

    update button works

--- main.js
+++ main.js
@@ -3,36 +3,11 @@ let canvas;
 window.onload = function() {
     canvas = document.getElementById("viewport");
     const ctx = canvas.getContext("2d");
-
     const size = Number(document.getElementById("viewport").getAttribute("width"));
     let imageData = ctx.getImageData(0,0,size,size);  
     
-    paint_it_black(imageData, size)
-
-    //generate//
-    
-    for (let y = 0; y < size; y++){
-        for (let x = -0; x < size; x++){
-            let r,g,b = 0;
-            
-            if (y > 0 && x > 0){
-
-                switch ((x%2 + y%2)) {
-                    case 2:
-                        r = Math.floor((y/64)*255);
-                        break;
-                    case 0:
-                        g = Math.floor((x/64)*255);
-                        break;
-                    default:
-                        b = 180 - Math.floor((x+y)/128*255);
-                }
-
-            set_pixel(imageData,x,y,r,g,b)
-            }  
-        }   
-    }
-    paint_canvas(imageData, ctx);
+    paint_it_black(imageData, size);
+    generate(size,imageData, ctx);
 }
 
 
@@ -72,4 +47,47 @@ function paint_it_black(imgData, size){
             set_pixel(imgData,x,y,0,0,0);
         }
     }
+}
+
+function generate(size,imageData,ctx){
+    for (let y = 0; y < size; y++){
+        for (let x = -0; x < size; x++){
+            let r,g,b = 0;
+            
+            if (y > 0 && x > 0){
+
+                switch ((x%2 + y%2)) {
+                    case 2:
+                        r = Math.floor((y/64)*255);
+                        break;
+                    case 0:
+                        g = Math.floor((x/64)*255);
+                        break;
+                    default:
+                        b = 180 - Math.floor((x+y)/128*255);
+                }
+
+            set_pixel(imageData,x,y,r,g,b);
+            }  
+        }   
+    }
+    paint_canvas(imageData, ctx);
+}
+
+function update(){
+    const size = Number(document.getElementById("viewport").getAttribute("width"));
+    console.log("updated the canvas")
+    const ctx = canvas.getContext("2d");
+    
+    let imageData = ctx.getImageData(0,0,size,size);  
+    set_pixel(imageData,random_position(size),random_position(size), random_color(), random_color(), random_color())
+    paint_canvas(imageData,ctx)
+}
+
+function random_position(size){
+    return Math.floor(Math.random()*size)
+}
+
+function random_color(){
+    return Math.floor(Math.random()*255)
 }
