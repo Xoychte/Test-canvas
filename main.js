6c605b7d44c20756eb1da45241d9c8451f48efd0
Gabriel <gabriel@mazure.fr>
2024-01-11 19:14:28 +0100

    SNOW PILES

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 "use strict";
 let canvas;
+
+let BLACK = [0,0,0,255];
+let WHITE = [255,255,255,255];
+
 window.onload = function() {
     canvas = document.getElementById("viewport");
     const ctx = canvas.getContext("2d");
     const size = Number(document.getElementById("viewport").getAttribute("width"));
     let imageData = ctx.getImageData(0,0,size,size);  
-    
+
     paint_it_black(imageData, size);
-    generate(size,imageData, ctx);
+    generate_initial(size,imageData, ctx);
+
 }
 
 
@@ -49,40 +54,66 @@ function paint_it_black(imgData, size){
     }
 }
 
-function generate(size,imageData,ctx){
+function generate_initial(size,imageData,ctx){
     for (let y = 0; y < size; y++){
-        for (let x = -0; x < size; x++){
-            let r,g,b = 0;
-            
-            if (y > 0 && x > 0){
-
-                switch ((x%2 + y%2)) {
-                    case 2:
-                        r = Math.floor((y/64)*255);
-                        break;
-                    case 0:
-                        g = Math.floor((x/64)*255);
-                        break;
-                    default:
-                        b = 180 - Math.floor((x+y)/128*255);
+        for (let x = 0; x < size; x++){
+            let r = 255;
+            let g = 255;
+            let b = 255;
+            let rand = Math.random()
+            if (y===0 && rand > 0.99){
+                r = 0
+                b = 0
+                g = 0
                 }
-
             set_pixel(imageData,x,y,r,g,b);
             }  
         }   
-    }
+    
     paint_canvas(imageData, ctx);
 }
 
 function update(){
     const size = Number(document.getElementById("viewport").getAttribute("width"));
-    console.log("updated the canvas")
     const ctx = canvas.getContext("2d");
-    
-    let imageData = ctx.getImageData(0,0,size,size);  
-    set_pixel(imageData,random_position(size),random_position(size), random_color(), random_color(), random_color())
-    paint_canvas(imageData,ctx)
+    let oldImageData = ctx.getImageData(0,0,size,size); 
+    let newImageData = ctx.getImageData(0,0,size,size); 
+
+    /// Make the pixels fall
+    for (let y = size; y >= 0; y--){
+        for (let x = 0; x < size; x++){
+            let oldPixel = get_pixel(oldImageData.data,x,y);
+            if (oldPixel.toString() == BLACK.toString()){
+                if (size-y > piles[x]){
+                    set_pixel(newImageData,x,y,255,255,255);
+                    set_pixel(newImageData,x,y+1,0,0,0);
+                    if (size-y-1 == piles[x]){
+                        piles[x]++
+                    }
+                }
+
+            }
+            if (oldPixel.toString() == BLACK.toString() &&y<size-1){
+                
+            } else {
+                set_pixel(newImageData,x,y,oldPixel[0], oldPixel[1], oldPixel[2])
+            }
+        }
+
+    }
+
+    /// Add new pixels
+    let firstRowSpace = 0;
+    for (let x = 0; x<size; x++){
+        let oldPixel = get_pixel(oldImageData.data,x,0)
+        if (oldPixel.toString() == WHITE.toString()){
+            firstRowSpace+=1;
+        }
+    }
+    set_pixel(newImageData,random_position(firstRowSpace),1,0,0,0);
+    paint_canvas(newImageData,ctx);
 }
+
 
 function random_position(size){
     return Math.floor(Math.random()*size)
@@ -91,3 +122,17 @@ function random_position(size){
 function random_color(){
     return Math.floor(Math.random()*255)
 }
+
+function start_regenerating(){
+    setInterval(update,1)
+}
+
+function create_piles(size){
+    let piles =[];
+    for (let i=0; i<size; i++){
+        piles.push(1);
+    }
+    return piles;
+}
+
+let piles = create_piles(64)
